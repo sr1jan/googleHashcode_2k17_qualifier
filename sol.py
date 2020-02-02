fdf292eb1c69107f4e81adc21cae41a8a7850456
sr1jan <srofficialsingh@gmail.com>
2020-02-02 16:40:11 +0530

    Update informative code

--- sol.py
+++ sol.py
@@ -18,16 +18,27 @@ if __name__ ==  '__main__':
         end_pts[r_info[1]][2].append((r_info[0], r_info[2]))
 
 
-
-    no_c = [0,[]]
+    # printing end_point, number of server connected
+    # dict[key_value][list_index][sub_list_index]
     for ep in end_pts:
-        if end_pts[ep][0][1] is 0:
-            no_c[0] += 1
-            no_c[1].append(ep)
+        print(f"{ep}, {end_pts[ep][0][1]}")
 
-    print("Num_of_endPts: ",len(end_pts))
-    print("Num_of_caches: "caches)
-    print("Not connected to any cache: "no_c)
+    # no_c = [0,[]]
+    # for ep in end_pts:
+    #     if end_pts[ep][0][1] is 0:
+    #         no_c[0] += 1
+    #         no_c[1].append(ep)
+
+    #     if len(end_pts[ep][1]) is not 0:
+    #         c_lats = [i[1] for i in end_pts[ep][1]]
+    #         cmin = min(c_lats)
+    #         cmax = max(c_lats)
+    #         if cmax > end_pts[ep][0][0]:
+    #             print(f"{ep} ~ dc_lat:{end_pts[ep][0]}, cmin_lat:{cmin}, cmax_lat:{cmax}")
+
+    # print("Num_of_endPts:",len(end_pts))
+    # print("Num_of_caches:",caches)
+    # print("Not connected to any cache:",no_c)
 
 
 
